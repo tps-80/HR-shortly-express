9cfe8dc6d3d2296858fc2ce80542e768068ef80a
Thomas Sorensen <thomaspsorensen@gmail.com>
2015-12-14 11:06:18 -0800

    fixed spacing

--- shortly.js
+++ shortly.js
@@ -22,7 +22,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
-
 app.get('/', 
 function(req, res) {
   res.render('index');
